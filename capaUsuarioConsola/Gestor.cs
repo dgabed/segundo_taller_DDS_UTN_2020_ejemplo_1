f5c0f255e38674931231d84059e29567083617ae
dgabed <dtgabriel@gmail.com>
2020-05-12 17:16:48 -0300

    Ejemplo con la parte de colecciones finalizado, funcionando OK

---
+++ capaUsuarioConsola/Gestor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capaDominio;
+
+namespace capaUsuarioConsola
+{
+    class Gestor
+    {
+        private ArrayList lista;
+        private List<Empleado> listaGenEmpleados;
+
+        public Gestor()
+        {
+            lista = new ArrayList();
+            for (int i = 0; i < 5; i++)
+                lista.Add(new Administrativo(i + 1, "Administrativo: " + (i + 1), 30000, 8));
+
+            for(int i = 5; i<10; i++)
+                lista.Add(new Profesional(i + 1, "Profesional: " + (i + 1), 60000,20000));
+
+            listaGenEmpleados = new List<Empleado>();
+            for (int i = 0; i < 5; i++)
+                listaGenEmpleados.Add(new Administrativo(i + 1, "Administrativo: " + (i + 1), 30000, 8));
+
+            for (int i = 5; i < 10; i++)
+                listaGenEmpleados.Add(new Profesional(i + 1, "Profesional: " + (i + 1), 60000, 20000));
+        }
+
+        public void conFor()
+        {
+            Empleado e;
+            for(int i = 0; i<lista.Count; i++)
+            {
+                e = (Empleado)lista[i];
+                Console.WriteLine(e.getNombre());
+            }
+        }
+
+        public void conForGen()
+        {
+            Empleado e;
+            for (int i = 0; i < listaGenEmpleados.Count; i++)
+            {
+                e = listaGenEmpleados[i];
+                Console.WriteLine(e.getNombre());
+            }
+        }
+
+        public void conIterator()
+        {
+            Empleado e;
+            IEnumerator iterador = lista.GetEnumerator();
+
+            while(iterador.MoveNext())
+            {
+                e = (Empleado)iterador.Current;
+                Console.WriteLine(e.getNombre());
+            }
+        }
+
+        public void conIteratorGen()
+        {
+            Empleado e;
+            IEnumerator<Empleado> iterador = listaGenEmpleados.GetEnumerator();
+
+            while (iterador.MoveNext())
+            {
+                e = iterador.Current;
+                Console.WriteLine(e.getNombre());
+            }
+        }
+
+        public void conForEach()
+        {
+            foreach(Empleado e in listaGenEmpleados)
+            {
+                Console.WriteLine(e.getNombre());
+            }
+        }
+
+    }
+}
--- capaUsuarioConsola/Program.cs
+++ capaUsuarioConsola/Program.cs
@@ -10,6 +10,28 @@ namespace capaUsuarioConsola
     {
         static void Main(string[] args)
         {
+            Gestor g = new Gestor();
+
+            Console.WriteLine("Muestra lista con For: ");
+            g.conFor();
+            Console.ReadLine();
+
+            Console.WriteLine("Muestra lista genérica con For: ");
+            g.conForGen();
+            Console.ReadLine();
+
+            Console.WriteLine("Muestra lista con Iterador: ");
+            g.conIterator();
+            Console.ReadLine();
+
+            Console.WriteLine("Muestra lista genérica con Iterador: ");
+            g.conIteratorGen();
+            Console.ReadLine();
+
+            Console.WriteLine("Muestra lista genérica con ForEach: ");
+            g.conForEach();
+            Console.ReadLine();
+
         }
     }
 }
