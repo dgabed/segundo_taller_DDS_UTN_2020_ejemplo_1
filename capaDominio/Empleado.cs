30fa63dc3e83297cda6984ca58b72c13d22baeff
dgabed <dtgabriel@gmail.com>
2020-05-12 10:24:14 -0300

    Capa de dominio agregada

---
+++ capaDominio/Empleado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDominio
+{
+    public abstract class Empleado
+    {
+        protected int legajo;
+        protected String nombre;
+        protected double sueldoBasico;
+
+        public Empleado(int legajo, String nombre, double sueldoBasico)
+        {
+            this.legajo = legajo;
+            this.nombre = nombre;
+            this.sueldoBasico = sueldoBasico;
+        }
+
+        public Empleado()
+        {
+            this.legajo = 0;
+            this.nombre = "";
+            this.sueldoBasico = 0;
+        }
+
+        public int getLegajo()
+        {
+            return this.legajo;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public double getSueldoBasico()
+        {
+            return this.sueldoBasico;
+        }
+
+        public abstract double calcularSueldo();
+    }
+}
---
+++ capaDominio/Profesional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDominio
+{
+    public class Profesional : Empleado
+    {
+        private double plus;
+
+        public Profesional(int legajo, String nombre, double sueldoBasico, double plus) : base(legajo, nombre, sueldoBasico)
+        {
+            this.plus = plus;
+
+        }
+
+        public override double calcularSueldo()
+        {
+            return this.getSueldoBasico() + plus;
+        }
+    }
+}
