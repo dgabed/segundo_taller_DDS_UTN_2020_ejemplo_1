a59a741c5f5c2a4a680599e2dc39c43583b49514
dgabed <dtgabriel@gmail.com>
2020-05-13 15:50:55 -0300

    Agregadas Clase Empleado concreta de Ejemplo

---
+++ capaDominioClaseConcreta/Administrativo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDominioClaseConcreta
+{
+    public class Administrativo : Empleado
+    {
+        private int horasExtra;
+
+        public Administrativo(int legajo, String nombre, double sueldoBasico, int horasExtra): base(legajo, nombre, sueldoBasico)
+        {
+            this.horasExtra = horasExtra;
+        }
+
+        public override double calcularSueldo()
+        {
+            return this.sueldoBasico + this.horasExtra * this.sueldoBasico * 0.05;
+            //throw new NotImplementedException();
+        }
+       
+    }
+}
---
+++ capaDominioClaseConcreta/Empleado.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDominioClaseConcreta
+{
+    public class Empleado : Impuesto
+    {
+        protected int legajo;
+        protected String nombre;
+        protected double sueldoBasico;
+
+        public Empleado(int legajo, String nombre, double sueldoBasico)
+        {
+            this.legajo = legajo;
+            this.nombre = nombre;
+            this.sueldoBasico = sueldoBasico;
+        }
+
+        public Empleado()
+        {
+            this.legajo = 0;
+            this.nombre = "";
+            this.sueldoBasico = 0;
+        }
+
+        public int getLegajo()
+        {
+            return this.legajo;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public double getSueldoBasico()
+        {
+            return this.sueldoBasico;
+        }
+
+        public virtual double calcularSueldo()
+        {
+            return this.sueldoBasico;
+        }
+
+        public double calcularImpuesto()
+        {
+            return this.sueldoBasico * 0.05;
+        }
+    }
+}
